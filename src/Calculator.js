6d430b17305f65b0a073eaa2a13cc8397489ddd6
achrafthedev <achrafchardoudi2002@gmail.com>
2025-01-09 14:10:02 +0100

    add advanced functions

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,14 +4,27 @@ import './App.css';
 const Calculator = () => {
     const [input, setInput] = useState('');
 
+    // Handle button clicks
     const handleClick = (value) => {
-        setInput(input + value);
+        if (value === '±') {
+            setInput((prev) => (prev ? (-1 * parseFloat(prev)).toString() : ''));
+        } else if (value === '√') {
+            setInput((prev) => Math.sqrt(parseFloat(prev)).toString());
+        } else if (value === 'CE') {
+            setInput(input.slice(0, -1));
+        } else if (value === '%') {
+            setInput((prev) => (parseFloat(prev) / 100).toString());
+        } else {
+            setInput(input + value);
+        }
     };
 
+    // Clear input
     const handleClear = () => {
         setInput('');
     };
 
+    // Evaluate the expression
     const handleCalculate = () => {
         try {
             setInput(eval(input).toString());
@@ -31,31 +44,38 @@ const Calculator = () => {
                     readOnly
                 />
                 <div className="row">
+                    <button className="btn btn-danger col-3" onClick={handleClear}>C</button>
+                    <button className="btn btn-secondary col-3" onClick={() => handleClick('CE')}>CE</button>
+                    <button className="btn btn-secondary col-3" onClick={() => handleClick('%')}>%</button>
+                    <button className="btn btn-warning col-3" onClick={() => handleClick('/')}>/</button>
+                </div>
+                <div className="row">
                     <button className="btn btn-light col-3" onClick={() => handleClick('7')}>7</button>
                     <button className="btn btn-light col-3" onClick={() => handleClick('8')}>8</button>
                     <button className="btn btn-light col-3" onClick={() => handleClick('9')}>9</button>
-                    <button className="btn btn-warning col-3" onClick={() => handleClick('/')}>/</button>
+                    <button className="btn btn-warning col-3" onClick={() => handleClick('*')}>*</button>
                 </div>
                 <div className="row">
                     <button className="btn btn-light col-3" onClick={() => handleClick('4')}>4</button>
                     <button className="btn btn-light col-3" onClick={() => handleClick('5')}>5</button>
                     <button className="btn btn-light col-3" onClick={() => handleClick('6')}>6</button>
-                    <button className="btn btn-warning col-3" onClick={() => handleClick('*')}>*</button>
+                    <button className="btn btn-warning col-3" onClick={() => handleClick('-')}>-</button>
                 </div>
                 <div className="row">
                     <button className="btn btn-light col-3" onClick={() => handleClick('1')}>1</button>
                     <button className="btn btn-light col-3" onClick={() => handleClick('2')}>2</button>
                     <button className="btn btn-light col-3" onClick={() => handleClick('3')}>3</button>
-                    <button className="btn btn-warning col-3" onClick={() => handleClick('-')}>-</button>
+                    <button className="btn btn-warning col-3" onClick={() => handleClick('+')}>+</button>
                 </div>
                 <div className="row">
+                    <button className="btn btn-light col-3" onClick={() => handleClick('±')}>±</button>
                     <button className="btn btn-light col-3" onClick={() => handleClick('0')}>0</button>
                     <button className="btn btn-light col-3" onClick={() => handleClick('.')}>.</button>
                     <button className="btn btn-success col-3" onClick={handleCalculate}>=</button>
-                    <button className="btn btn-warning col-3" onClick={() => handleClick('+')}>+</button>
                 </div>
-                <div className="row mt-3">
-                    <button className="btn btn-danger col-12" onClick={handleClear}>Clear</button>
+                <div className="row">
+                    <button className="btn btn-info col-6" onClick={() => handleClick('√')}>√ (Square Root)</button>
+                    <button className="btn btn-dark col-6" onClick={() => handleClick('^2')}>^2 (Square)</button>
                 </div>
             </div>
         </div>
